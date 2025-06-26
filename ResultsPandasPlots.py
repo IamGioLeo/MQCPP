67ab57a6cdde90c1632c749928efdcecb1877fb4
LEO <gioleo999@gmail.com>
2025-06-26 16:26:57 +0200

    removed useless code and comments

--- ResultsPandasPlots.py
+++ ResultsPandasPlots.py
@@ -4,7 +4,7 @@ from matplotlib.colors import LogNorm
 import seaborn as sns
 import numpy as np
 
-actual_csv_path = 'not_git/first_real_test_ILS.csv'
+
 test_ILS_path = '01_final_test_ILS.csv'
 test_gurobi_path = '01_final_test_gurobi.csv'
 scalability_ILS_path = '01_final_scalability_ILS.csv'
@@ -17,246 +17,19 @@ def csv_to_table(ILS_path, gurobi_path):
 
     df = pd.concat([df1, df2], ignore_index=True)
     df["Graph"] = df["Graph"].str.replace("instances/", "", regex=False)
-    #df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
     df["Time"] = df["Time"].apply(lambda x: np.trunc(x * 1000) / 1000)
     df["Initial_solution_time"] = df["Initial_solution_time"].apply(lambda x: np.trunc(x * 1000) / 1000)
     df["Best_solution_time"] = df["Best_solution_time"].apply(lambda x: np.trunc(x * 1000) / 1000)
 
 
-    df_to_print = df[["Graph", "Gamma", "Time", "Initial_solution_time","Best_solution_time","Best_solution_size"]].rename(columns={"Initial_solution_time": "IS_time","Best_solution_time": "S_time","Best_solution_size": "S"})
+    df_to_print = df[["Graph", "Gamma", "Time", "Initial_solution_time","Best_solution_time","Best_solution_size", "Solver"]].rename(columns={"Initial_solution_time": "IS_time","Best_solution_time": "S_time","Best_solution_size": "S"})
 
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
-    columns_to_print = ["Graph", "Gamma", "Time", "IS_time", "S_time", "S"]
+    columns_to_print = ["Graph", "Gamma", "Time", "IS_time", "S_time", "S", "Solver"]
     print(df_to_print[columns_to_print])
 
 csv_to_table(test_ILS_path,test_gurobi_path)
-
-def boxplot_log_time_for_gamma(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-    df["Gamma"] = df["Gamma"].astype(float)
-
-    plt.figure(figsize=(8, 5))
-    sns.boxplot(x="Gamma", y="Time", data=df)
-
-    plt.yscale("log")
-    plt.title("Boxplot del Tempo (scala log) per Gamma")
-    plt.xlabel("Gamma")
-    plt.ylabel("log(Time in seconds)")
-    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-    plt.tight_layout()
-    plt.show()
-
-
-#boxplot_log_time_for_gamma(actual_csv_path)
-
-
-def boxplot_log_time_for_number_of_nodes(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-
-    df["#nodes"] = df["#nodes"].astype(int)
-
-    plt.figure(figsize=(8, 5))
-    sns.boxplot(x="#nodes", y="Time", data=df)
-
-    plt.yscale("log")
-    plt.title("Boxplot del Tempo (scala log) per numero di nodi")
-    plt.xlabel("#nodes")
-    plt.ylabel("log(Time in seconds)")
-    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-    plt.tight_layout()
-    plt.show()
-
-
-#boxplot_log_time_for_number_of_nodes(actual_csv_path)
-
-
-def boxplot_log_time_for_number_of_edges(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-
-    df["#edges"] = df["#edges"].astype(int)
-
-    plt.figure(figsize=(8, 5))
-    sns.boxplot(x="#edges", y="Time", data=df)
-
-    plt.yscale("log")
-    plt.title("Boxplot del Tempo (scala log) per numero di nodi")
-    plt.xlabel("#edges")
-    plt.ylabel("log(Time in seconds)")
-    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-    plt.tight_layout()
-    plt.show()
-
-
-#boxplot_log_time_for_number_of_edges(actual_csv_path)
-
-
-def time_scatterplot_for_nodes_and_gamma(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-
-    df["Gamma"] = df["Gamma"].astype(float)
-    df["#nodes"] = df["#nodes"].astype(int)
-
-    # Crea scatter plot
-    plt.figure(figsize=(8, 6))
-    sc = plt.scatter(
-        x=df["Gamma"],
-        y=df["#nodes"],
-        c=df["Time"],
-        cmap="coolwarm",
-        norm=LogNorm(),
-        edgecolors='face',
-        s=50  # dimensione dei cerchietti
-    )
-
-    plt.yscale("log")
-
-    cbar = plt.colorbar(sc)
-    cbar.set_label("Tempo (secondi)")
-
-    plt.xlabel("Gamma")
-    plt.ylabel("Numero di nodi")
-    plt.title("Scatter: Tempo in funzione di Gamma e Numero di Nodi")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-#time_scatterplot_for_nodes_and_gamma(actual_csv_path)
-
-
-def time_scatterplot_for_nodes_and_edges(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-
-    df["#edges"] = df["#edges"].astype(int)
-    df["#nodes"] = df["#nodes"].astype(int)
-
-    # Crea scatter plot
-    plt.figure(figsize=(8, 6))
-    sc = plt.scatter(
-        x=df["#nodes"],
-        y=df["#edges"],
-        c=df["Time"],
-        cmap="coolwarm",
-        norm=LogNorm(),
-        edgecolors='face',
-        s=50  # dimensione dei cerchietti
-    )
-
-    plt.yscale("log")
-
-    cbar = plt.colorbar(sc)
-    cbar.set_label("Tempo (secondi)")
-
-    plt.xlabel("Numero di nodi")
-    plt.ylabel("Numero di archi")
-    plt.title("Scatter: Tempo in funzione del numero di nodi e del numero di archi")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-#time_scatterplot_for_nodes_and_edges(actual_csv_path)
-
-
-def time_scatterplot_for_nodes_and_gamma_labels(csv_path):
-    df = pd.read_csv(csv_path)
-    # df["Time"] = pd.to_timedelta(df["Time"]).dt.total_seconds()
-    df["Time"] = df["Time"].astype(float)
-    df["Gamma"] = df["Gamma"].astype(str)
-    df["#nodes"] = df["#nodes"].astype(int)
-
-    unique_gammas = sorted(df["Gamma"].unique())
-    palette = sns.color_palette("tab10", n_colors=len(unique_gammas))  # o "Set1", "Dark2", ecc.
-    color_map = {gamma: palette[i] for i, gamma in enumerate(unique_gammas)}
-    df["Color"] = df["Gamma"].map(color_map)
-
-    # Crea scatter plot
-    plt.figure(figsize=(8, 6))
-    for gamma in unique_gammas:
-        subset = df[df["Gamma"] == gamma]
-        plt.scatter(
-            subset["#nodes"],
-            subset["Time"],
-            label=f"Gamma = {gamma}",
-            color=color_map[gamma],
-            edgecolors='black',
-            s=50
-        )
-
-    plt.yscale("log")
-
-    plt.xlabel("Numero di nodi")
-    plt.ylabel("Tempo (secondi)")
-    plt.title("Scatter: Tempo in funzione di Numero di Nodi e Gamma")
-    plt.legend(title="Gamma")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-#time_scatterplot_for_nodes_and_gamma_labels(actual_csv_path)
-
-
-def time_for_density_gamma_labels(csv_path):
-    df = pd.read_csv(csv_path)
-    df["Density"] = 2 * df["#edges"] / (df["#nodes"] * (df["#nodes"] - 1))
-
-    # Conversione delle colonne necessarie
-    df["Time"] = df["Time"].astype(float)
-
-    # Crea il grafico
-    plt.figure(figsize=(8, 6))
-    plt.scatter(
-        df["Density"],
-        df["Time"],
-        c=df["Gamma"],
-        cmap="tab10",
-        edgecolors='black',
-        s=50
-    )
-
-    plt.yscale("log")
-    plt.xlabel("Densità")
-    plt.ylabel("Tempo (secondi)")
-    plt.title("Scatter: Tempo in funzione della densità")
-    plt.grid(True)
-
-    cbar = plt.colorbar()
-    cbar.set_label("Gamma")
-
-    plt.tight_layout()
-    plt.show()
-
-#time_for_density_gamma_labels(actual_csv_path)
-
-
-
-def jointplot_time_vs_density(csv_path):
-    df = pd.read_csv(csv_path)
-    df["Density"] = 2 * df["#edges"] / (df["#nodes"] * (df["#nodes"] - 1))
-    df["Time"] = df["Time"].astype(float)
-
-    sns.jointplot(
-        x="Density", y="Time", data=df,
-        kind="reg",  # lineare regressione
-        height=8,
-        marginal_kws=dict(bins=30, fill=True)
-    )
-    plt.suptitle("Jointplot: Time vs Density with regression", y=1.02)
-    plt.show()
-
-#jointplot_time_vs_density(actual_csv_path)
 
 def best_solution_comparison_ILS_gurobi(ILS_path, gurobi_path):
     df1 = pd.read_csv(ILS_path)
@@ -279,7 +52,7 @@ def best_solution_comparison_ILS_gurobi(ILS_path, gurobi_path):
     plt.grid(True, axis='y')
     plt.show()
 
-best_solution_comparison_ILS_gurobi(scalability_ILS_path, scalability_gurobi_path)
+best_solution_comparison_ILS_gurobi(test_ILS_path, test_gurobi_path)
 
 def time_comparison_ILS_gurobi(ILS_path, gurobi_path):
     df1 = pd.read_csv(ILS_path)
@@ -303,4 +76,4 @@ def time_comparison_ILS_gurobi(ILS_path, gurobi_path):
     plt.grid(True, axis= 'y')
     plt.show()
 
-time_comparison_ILS_gurobi(scalability_ILS_path, scalability_gurobi_path)
+time_comparison_ILS_gurobi(test_ILS_path, test_gurobi_path)
--- Test.py
+++ Test.py
@@ -2,14 +2,7 @@ from Function import function
 import random
 import numpy as np
 
-#istanze problematiche: anna g 0.999; 'instances/494_bus.mtx'
-#instances_file_paths = ['instances/Memento.txt', 'instances/the_x_files.txt', 'instances/Alien_3.txt', 'instances/high-tech.txt', 'instances/karate.mtx', 'instances/mexican.paj', 'instances/Sawmill.net', 'instances/chesapeake.mtx', 'instances/Batman_Returns.txt', 'instances/Attiro.paj', 'instances/soc-dolphins.mtx', 'instances/SanJuanSur.paj', 'instances/jean.txt', 'instances/3-FullIns_3.txt', 'instances/david.txt', 'instances/myciel6.txt', 'instances/4-FullIns_3.txt', 'instances/anna.txt']
-                        # 'instances/494_bus.mtx'
-#instances_file_paths = ['instances/p-hat500-1.mtx', 'instances/p-hat500-2.mtx', 'instances/p-hat500-3.mtx', 'instances/662_bus.mtx', 'instances/keller5.mtx', 'instances/brock800-3.mtx', 'instances/email-dnc-corecipient.edges', 'instances/san1000.mtx', 'instances/p-hat1000-1.mtx', 'instances/p-hat1000-2.mtx', 'instances/p-hat1000-3.mtx', 'instances/email.edges', 'instances/polblogs.mtx', 'instances/p-hat1500-3.mtx', 'instances/p-hat1500-1.mtx', 'instances/p-hat1500-2.mtx', 'instances/C2000-5.mtx', 'instances/bcsstk13.mtx', 'instances/soc-hamsterster.edges', 'instances/data.mtx']
-#instances_file_paths = ['instances/Memento.txt', 'instances/the_x_files.txt', 'instances/Alien_3.txt']
-#instances_file_paths = ['instances/4-FullIns_3.txt', 'instances/Batman_Returns.txt','instances/brock800-3.mtx', 'instances/data.mtx', 'instances/494_bus.mtx', 'instances/bcsstk13.mtx']
 
-#fatte
 test_instances_file_paths = ['instances/Memento.txt', 'instances/the_x_files.txt', 'instances/high-tech.txt', 'instances/mexican.paj']
 
 scalability_instances_file_paths = ['instances/p-hat500-3.mtx','instances/662_bus.mtx', 'instances/brock800-3.mtx', 'instances/email-dnc-corecipient.edges', 'instances/Batman_Returns.txt', 'instances/4-FullIns_3.txt', 'instances/p-hat500-1.mtx'] # fatte
